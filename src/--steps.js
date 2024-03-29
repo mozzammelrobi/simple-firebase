ce07afd8952ccd278a05796add1df8b4adfc0423
mozzammelrobi <mozzammelfb@gmail.com>
2024-03-29 22:20:25 +0600

    first commit

---
+++ src/--steps.js
@@ -0,0 +1,9 @@
+/**
+ * 1. visit: console.firebase.googel.com
+ * 2. create project (skip google analystics)
+ * 3. Register app (create config)
+ * 4. install firebase: npm install firebase
+ * 5. add config file
+ * 6. DANGER: do not publish ro made firebase config to public by pushing to github
+ * 
+ */
