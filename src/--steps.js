02a99cb4cc0ac30a75b05b8557b946f9a5c4e679
mozzammelrobi <mozzammelfb@gmail.com>
2024-03-30 01:03:05 +0600

    Open  google login popup and display loggen user info, sign out and toggle sing in and out

--- src/--steps.js
+++ src/--steps.js
@@ -10,5 +10,7 @@
  * 9. Login.jsx: import getAuth from frebase/auth
  * 10. create const auth = getAuth(app)
  * 11. import GoogleAuthProvider and create a new provider
+ * 12. use signInWithPopUp and pass auth and provider
+ * 13. activate sign-in method (google, facebook, github, etc.)
  * 
  */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,44 @@
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { useState } from "react";
 import app from "../../firebase/firebase.init";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app)
-    const provider = new GoogleAuthProvider();
+    const provider = new GoogleAuthProvider()
 
     const handleGoogleSignIn = () => {
-        console.log('google mama is comming')
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const logingUser = result.user
+                console.log(logingUser?.photoURL)
+                setUser(logingUser)
+            })
     }
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result)
+                setUser(null)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    console.log(user?.photoURL)
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Google Login</button>
+            {user ?
+                <button onClick={handleSignOut}>sign OUt</button>:
+                <button onClick={handleGoogleSignIn}>Google Login</button> 
+            }
+            {user && <div>
+                <h3>user: {user.displayName}</h3>
+                <p>Email: {user.email}</p>
+                <img src={user.photoURL} alt="user img not fund" />
+            </div>}
         </div>
     );
 };
